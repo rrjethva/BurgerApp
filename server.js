8acfd9a5772f7c4c1e8a57805ff757e2cd1a534d
Ravi Jethva <rrjethva@gmail.com>
2019-11-14 22:35:41 -0500

    server.js file complete with requirements

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var PORT = process.env.PORT || 8080;
+
+var app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgers_controllers");
+app.use(routes);
+
+app.listen(PORT, function(){
+    console.log("Server listneing on: http://localhost:" + PORT);
+});
+
